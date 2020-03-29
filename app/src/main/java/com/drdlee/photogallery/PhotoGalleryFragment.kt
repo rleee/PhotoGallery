0ccf33890eb9785e714ceae02525d9c1a611fa41
rleee <danlee0150@gmail.com>
2020-03-29 12:54:02 +0800

    Simple retrofit implementation to enqueue flickr homepage

--- app/src/main/java/com/drdlee/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/drdlee/photogallery/PhotoGalleryFragment.kt
@@ -1,16 +1,48 @@
 package com.drdlee.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.drdlee.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "AppDebug"
 
 class PhotoGalleryFragment: Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        val flickrHomepageRequest: Call<String> = flickrApi.fetchContents()
+        
+        flickrHomepageRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "onFailure: Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "onResponse: Response received ${response.body()}")
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
---
+++ app/src/main/java/com/drdlee/photogallery/api/FlickrApi.kt
@@ -0,0 +1,10 @@
+package com.drdlee.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET("/")
+    fun fetchContents(): Call<String>
+}
